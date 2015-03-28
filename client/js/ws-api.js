eeef03cda5b3af1d4c1a2a93a8237d79849c2b2b
Tomas Lycken <tomas.lycken@jayway.com>
2015-03-28 23:35:01 +0100

    Update example client

--- client/js/ws-api.js
+++ client/js/ws-api.js
@@ -1,36 +1,52 @@
-function wsApi(url, port, msgHandler) {
-    websocket = new WebSocket('ws://' + url + ':' + port);
-    websocket.onopen = function(evt) {
-        onOpen(evt)
-    };
-    websocket.onclose = function(evt) {
-        onClose(evt)
-    };
-    websocket.onmessage = function(evt) {
-        onMessage(evt)
-    };
-    websocket.onerror = function(evt) {
-        onError(evt)
-    };
+function WebSocketService(url, port) {
+    this.openHandlers = [function(evt) {
+            console.log("Connected to ", url + ':' + port);
+        }];
+    this.closeHandlers = [function(evt) {
+            console.log("Disconnected from ", url + ':' + port);
+        }];
+    this.messageHandlers = [function(evt) {
+            console.log('Message received: ', evt.data);
+        }];
+    this.errorHandlers = [function(evt) {
+            console.log('ERROR: ', evt.data);
+        }];
 
     function onOpen(evt) {
-        console.log("Connected to ", url + ':' + port);
+        for (var i in this.openHandlers) {
+            this.openHandlers[i](evt);
+        }
     }
 
     function onClose(evt) {
-        console.log("Disconnected from ", url + ':' + port);
+        for (var i in this.closeHandlers) {
+            this.closeHandlers[i](evt);
+        }
     }
 
     function onMessage(evt) {
-        var data = JSON.parse(evt.data);
-        sensorData[data.client] = data.value;
+        for (var i = 0; i < this.messageHandlers.length; ++i) {
+            this.messageHandlers[i](evt);
+        }
     }
 
     function onError(evt) {
-        console.log('ERROR: ', evt.data);
+        for (var i in this.errorHandlers) {
+            this.errorHandlers[i](evt);
+        }
     }
 
-    var sensorData = {};
+    this.socket = new WebSocket('ws://' + url + ':' + port);
+    this.socket.onopen = onOpen;
+    this.socket.onclose = onClose;
+    this.socket.onmessage = onMessage.bind(this);
+    this.socket.onerror = onError;
 
-    return sensorData;
+    this.addMessageHandler = function(handler) {
+        this.messageHandlers.push(handler);
+    }
+
+    this.sendMessage = function(msg) {
+        this.socket.send(msg);
+    }
 }
